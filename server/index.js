135d5c4e1b3cf657c5c6d8ad74a11c27e25e27bc
cli16 <cli16@mail.wou.edu>
2021-06-07 16:45:38 -0700

    init connection to database

--- server/index.js
+++ server/index.js
@@ -4,4 +4,21 @@ import mongoose from 'mongoose'
 import cors from 'cors';
 
 const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors);
+
+const CONNECTION_URL = process.env.MONGODB_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log(`server is running on port ${PORT}`);
+    })
+    .catch((error) => {
+        console.log(error.message)
+    });
+
+//ignore error warning
+mongoose.set('useFindAndModify', false);
