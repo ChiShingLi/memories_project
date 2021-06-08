77bc6e98194ebdba90c8eee13d6ba05b87d2f8fb
cli16 <cli16@mail.wou.edu>
2021-06-07 17:05:40 -0700

    added controller folder to extract out route logics

---
+++ server/controllers/posts.js
@@ -0,0 +1,5 @@
+//routes logics
+
+export const getPosts = (req, res) => {
+    res.send('hello');
+}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
+import { getPosts } from '../controllers/posts.js'
+
 const router = express.Router();
 
 // GET /posts/
-router.get('/', (req, res) => {
-    res.send('hello');
-});
+router.get('/', getPosts);
 
 export default router;
