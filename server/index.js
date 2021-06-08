90969047c8faed6f53669057aae37da86eb5d2a7
cli16 <cli16@mail.wou.edu>
2021-06-07 17:01:47 -0700

    app listen & routings

--- server/index.js
+++ server/index.js
@@ -3,7 +3,12 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose'
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js'
+
 const app = express();
+
+// path: /posts/..., 
+app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -12,9 +17,13 @@ app.use(cors);
 const CONNECTION_URL = process.env.MONGODB_URL;
 const PORT = process.env.PORT || 5000;
 
+//connect to mongoDB
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        console.log(`server is running on port ${PORT}`);
+        //start listening from API
+        app.listen(PORT, () => {
+            console.log(`server is running on port ${PORT}`);
+        })
     })
     .catch((error) => {
         console.log(error.message)
